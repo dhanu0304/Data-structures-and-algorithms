dd6ce309e751f8b85391842581703c662d387114
Dhananjaya Roul <dhananjaya032007@gmail.com>
2026-04-09 08:49:40 +0530

    Added Updated Insert_node_at_begin.c file in Linked List folder

--- Linked List/Insert_node_at_begin.c
+++ Linked List/Insert_node_at_begin.c
@@ -12,6 +12,7 @@ struct node* insertAtBegin(struct node* head, int Data) {
     newNode->next = head;
     return newNode; // New head of the list
 }
+
 //insert at end
 struct node* insertAtEnd(struct node* head, int Data) {
     struct node* newNode = (struct node*)malloc(sizeof(struct node));
@@ -28,3 +29,23 @@ struct node* insertAtEnd(struct node* head, int Data) {
     return head; // Head remains unchanged
 } 
 
+//fixed position 
+struct node* insertPosition(struct node* head, int Data, int pos) {
+    struct node* newNode = (struct node*)malloc(sizeof(struct node));
+    newNode->data = Data;
+    if (pos == 1) {
+        newNode->next = head;
+        return newNode; // New head of the list
+    }
+    struct node* temp = head;
+    for (int i = 1; i < pos - 1 && temp != NULL; i++) {
+        temp = temp->next;
+    }
+    if (temp == NULL) {
+        free(newNode); // Position is out of bounds
+        return head; // No change to the list
+    }
+    newNode->next = temp->next;
+    temp->next = newNode;
+    return head; // Head remains unchanged
+}
